935faf5b0ab3fca7ed6247c3ab64a75b02a0a339
pah <pah@ucodev.org>
2016-08-29 02:34:29 +0100

    Added docker-compose for ndphp and ispconfig3

---
+++ docker/compose/ispconfig3/build/ispconfig3/files/ispconfig3_roundcube_config.inc.php
@@ -0,0 +1,6 @@
+<?php
+$rcmail_config['identity_limit'] = false;
+$rcmail_config['remote_soap_user'] = 'ROUNDCUBE_ISP_USERNAME';
+$rcmail_config['remote_soap_pass'] = 'ROUNDCUBE_ISP_PASSWORD';
+$rcmail_config['soap_url'] = 'https://ROUNDCUBE_HOST_FQDN:8080/remote/';
+?>
